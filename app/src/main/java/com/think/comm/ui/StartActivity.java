b0908d71b73cc50f0ad040e7294286189806505d
wuyi165 <273733055@qq.com>
2016-11-17 15:57:11 +0800

    init

---
+++ app/src/main/java/com/think/comm/ui/StartActivity.java
@@ -0,0 +1,23 @@
+package com.think.comm.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.think.player.LiveActivity;
+
+public class StartActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent  = new Intent(this,LiveActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ thinkplay/src/main/java/com/think/player/LiveActivity.java
@@ -0,0 +1,463 @@
+package com.think.player;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.Process;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.baidu.cyberplayer.core.BVideoView;
+
+
+/**
+ * Created by jun.wu on 2016/6/29.
+ */
+public class LiveActivity extends Activity implements BVideoView.OnPreparedListener, BVideoView.OnCompletionListener,
+        BVideoView.OnErrorListener, BVideoView.OnInfoListener, BVideoView.OnPlayingBufferCacheListener,View.OnClickListener
+{
+
+    private final String TAG = "LiveActivity";
+
+    /**
+     * 您的AK
+     * 请到http://console.bce.baidu.com/iam/#/iam/accesslist获取
+     */
+    private String AK = "b0f03099694d45ed87160916a978e901";
+
+    private String mVideoSource = null;
+
+    private ImageView mPlaybtn = null;
+    private ImageView mFullscreen =null;
+    private LinearLayout mController = null;
+    private SeekBar mProgress = null;
+    private TextView mDuration = null;
+    private TextView mCurrPostion = null;
+
+    private boolean isFullscreen = true;
+    private  enum  PLAYER_EVENT{
+        PLAYER_BUFFER,PLAYER_PLAYING
+    }
+    private PLAYER_EVENT   mPlayerEvent = PLAYER_EVENT.PLAYER_BUFFER;
+    /**
+     * 记录播放位置
+     */
+    private int mLastPos = 0;
+
+    /**
+     * 播放状态
+     */
+    private  enum PLAYER_STATUS {
+        PLAYER_IDLE, PLAYER_PREPARING, PLAYER_PREPARED,
+    }
+
+    private PLAYER_STATUS mPlayerStatus = PLAYER_STATUS.PLAYER_IDLE;
+
+    private BVideoView mVV = null;
+    private EventHandler mEventHandler;
+    private HandlerThread mHandlerThread;
+    private final Object SYNC_Playing = new Object();
+
+    private PowerManager.WakeLock mWakeLock = null;
+    private static final String POWER_LOCK = "VideoViewPlayingActivity";
+
+    private boolean mIsHwDecode = false;
+    private final int EVENT_PLAY = 0;
+    private final int UI_EVENT_UPDATE_CURRPOSITION = 1;
+
+    class EventHandler extends Handler {
+        public EventHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case EVENT_PLAY:
+                    /**
+                     * 如果已经播放了，等待上一次播放结束
+                     */
+                    if (mPlayerStatus != PLAYER_STATUS.PLAYER_IDLE) {
+                        synchronized (SYNC_Playing) {
+                            try {
+                                SYNC_Playing.wait();
+                                Log.v(TAG, "wait player status to idle");
+                            } catch (InterruptedException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    /**
+                     * 设置播放url
+                     */
+                    mVV.setVideoPath(mVideoSource);
+
+                    /**
+                     * 续播，如果需要如此
+                     */
+                    if (mLastPos > 0) {
+                        mVV.seekTo(mLastPos);
+                        mLastPos = 0;
+                    }
+
+                    /**
+                     * 显示或者隐藏缓冲提示
+                     */
+                    mVV.showCacheInfo(true);
+
+                    /**
+                     * 开始播放
+                     */
+                    mVV.start();
+                    mPlayerStatus = PLAYER_STATUS.PLAYER_PREPARING;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    Handler mUIHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                /**
+                 * 更新进度及时间
+                 */
+                case UI_EVENT_UPDATE_CURRPOSITION:
+                    int currPosition = mVV.getCurrentPosition();
+                    int duration = mVV.getDuration();
+                    updateTextViewWithTimeFormat(mCurrPostion, currPosition);
+                    updateTextViewWithTimeFormat(mDuration, duration);
+                    mProgress.setMax(duration);
+                    if (mVV.isPlaying()){
+                        mProgress.setProgress(currPosition);
+                        //mPlaybtn.setVisibility(View.GONE);
+                    }
+                    mUIHandler.sendEmptyMessageDelayed(UI_EVENT_UPDATE_CURRPOSITION, 200);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, POWER_LOCK);
+
+        mIsHwDecode = getIntent().getBooleanExtra("isHW", false);
+        Uri uriPath = getIntent().getData();
+        if (null != uriPath) {
+            String scheme = uriPath.getScheme();
+            if (null != scheme) {
+                mVideoSource = uriPath.toString();
+            } else {
+                mVideoSource = uriPath.getPath();
+            }
+        }
+
+        if(mVideoSource==null){
+            mVideoSource = getIntent().getStringExtra("url");
+        }
+
+        if(mVideoSource==null){
+            mVideoSource  = "http://devimages.apple.com/iphone/samples/bipbop/gear4/prog_index.m3u8";
+        }
+        initUI();
+
+        /**
+         * 开启后台事件处理线程
+         */
+        mHandlerThread = new HandlerThread("event handler thread",Process.THREAD_PRIORITY_BACKGROUND);
+        mHandlerThread.start();
+        mEventHandler = new EventHandler(mHandlerThread.getLooper());
+
+    }
+
+    /**
+     * 初始化界面
+     */
+    private void initUI() {
+        mPlaybtn = (ImageView)findViewById(R.id.play);
+        mController = (LinearLayout)findViewById(R.id.layout_bottom);
+        mProgress = (SeekBar)findViewById(R.id.media_progress);
+        mDuration = (TextView)findViewById(R.id.time_total);
+        mCurrPostion = (TextView)findViewById(R.id.time_current);
+        mFullscreen  =(ImageView)findViewById(R.id.fullscreen);
+        registerCallbackForControl();
+
+        /**
+         * 设置ak
+         */
+        BVideoView.setAK(AK);
+        //BVideoView.setAKSK("s4vBXXOhalluI6bjTRE1PGO0", "UmXye3oDpeW7bj4HTUf9Aihyl7jzMEIE");
+        /**
+         *获取BVideoView对象
+         */
+        mVV = (BVideoView) findViewById(R.id.video_view);
+
+        /**
+         * 注册listener
+         */
+        mVV.setOnPreparedListener(this);
+        mVV.setOnCompletionListener(this);
+        mVV.setOnErrorListener(this);
+        mVV.setOnInfoListener(this);
+
+        /**
+         * 设置解码模式
+         */
+        mVV.setDecodeMode(mIsHwDecode?BVideoView.DECODE_HW:BVideoView.DECODE_SW);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+            if(v.getId() == R.id.play) {
+                if (mPlayerEvent == PLAYER_EVENT.PLAYER_BUFFER) {
+                    // Log.e(TAG,"is buffer.....");
+                    return;
+                }
+                if (mVV.isPlaying()) {
+                    mPlaybtn.setImageResource(R.drawable.click_play_selector);
+                    // 暂停播放
+                    mVV.pause();
+                } else {
+                    mPlaybtn.setImageResource(R.drawable.click_pause_selector);
+                    // 继续播放
+                    mVV.resume();
+                }
+            }
+        if(v.getId() == R.id.fullscreen) {
+            int action = isFullscreen ? View.SYSTEM_UI_FLAG_VISIBLE : View.SYSTEM_UI_FLAG_FULLSCREEN;
+            v.setSystemUiVisibility(action);
+            isFullscreen = !isFullscreen;
+            int src = isFullscreen ? R.drawable.shrink : R.drawable.enlarge;
+            ImageView fullscreen = (ImageView) v;
+            fullscreen.setImageResource(src);
+        }
+
+    }
+
+
+    /**
+     * 为控件注册回调处理函数
+     */
+    private void registerCallbackForControl(){
+
+//        mPlaybtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if (mVV.isPlaying()) {
+//                    mPlaybtn.setImageResource(R.drawable.click_play_selector);
+//                    /**
+//                     * 暂停播放
+//                     */
+//                    mVV.pause();
+//                } else {
+//                    mPlaybtn.setImageResource(R.drawable.click_pause_selector);
+//                    /**
+//                     * 继续播放
+//                     */
+//                    mVV.resume();
+//                }
+//            }
+//        });
+
+
+
+        SeekBar.OnSeekBarChangeListener osbc1 = new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress,  boolean fromUser) {
+                updateTextViewWithTimeFormat(mCurrPostion, progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                /**
+                 * SeekBar开始seek时停止更新
+                 */
+                mUIHandler.removeMessages(UI_EVENT_UPDATE_CURRPOSITION);
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int iseekPos = seekBar.getProgress();
+                 //SeekBark完成seek时执行seekTo操作并更新界面
+                mVV.seekTo(iseekPos);
+                Log.v(TAG, "seek to " + iseekPos);
+                mUIHandler.sendEmptyMessage(UI_EVENT_UPDATE_CURRPOSITION);
+            }
+        };
+        mProgress.setOnSeekBarChangeListener(osbc1);
+    }
+
+
+
+    private void updateTextViewWithTimeFormat(TextView view, int second){
+        int hh = second / 3600;
+        int mm = second % 3600 / 60;
+        int ss = second % 60;
+        String strTemp = null;
+        if (0 != hh) {
+            strTemp = String.format("%02d:%02d:%02d", hh, mm, ss);
+        } else {
+            strTemp = String.format("%02d:%02d", mm, ss);
+        }
+        view.setText(strTemp);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        /**
+         * 在停止播放前 你可以先记录当前播放的位置,以便以后可以续播
+         */
+        if (mPlayerStatus == PLAYER_STATUS.PLAYER_PREPARED) {
+            mLastPos = mVV.getCurrentPosition();
+            mVV.stopPlayback();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        Log.v(TAG, "onResume");
+        if (null != mWakeLock && (!mWakeLock.isHeld())) {
+            mWakeLock.acquire();
+        }
+        /**
+         * 发起一次播放任务,当然您不一定要在这发起
+         */
+        mEventHandler.sendEmptyMessage(EVENT_PLAY);
+    }
+
+    private long mTouchTime;
+    private boolean barShow = true;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // TODO Auto-generated method stub
+        if (event.getAction() == MotionEvent.ACTION_DOWN)
+            mTouchTime = System.currentTimeMillis();
+        else if (event.getAction() == MotionEvent.ACTION_UP) {
+            long time = System.currentTimeMillis() - mTouchTime;
+            if (time < 400) {
+                updateControlBar(!barShow);
+            }
+        }
+        return true;
+    }
+
+    public void updateControlBar(boolean show) {
+        if (show) {
+            mController.setVisibility(View.VISIBLE);
+            mPlaybtn.setVisibility(View.VISIBLE);
+        } else {
+            mController.setVisibility(View.INVISIBLE);
+            mPlaybtn.setVisibility(View.INVISIBLE);
+        }
+        barShow = show;
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        /**
+         * 退出后台事件处理线程
+         */
+        mHandlerThread.quit();
+    }
+
+    @Override
+    public boolean onInfo(int what, int extra) {
+        // TODO Auto-generated method stub
+        switch(what){
+            /**
+             * 开始缓冲
+             */
+            case BVideoView.MEDIA_INFO_BUFFERING_START:
+                mPlayerEvent = PLAYER_EVENT.PLAYER_BUFFER;
+                break;
+            /**
+             * 结束缓冲
+             */
+            case BVideoView.MEDIA_INFO_BUFFERING_END:
+                mPlayerEvent = PLAYER_EVENT.PLAYER_PLAYING;
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 当前缓冲的百分比， 可以配合onInfo中的开始缓冲和结束缓冲来显示百分比到界面
+     */
+    @Override
+    public void onPlayingBufferCache(int percent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * 播放出错
+     */
+    @Override
+    public boolean onError(int what, int extra) {
+        // TODO Auto-generated method stub
+        Log.v(TAG, "onError");
+        synchronized (SYNC_Playing) {
+            SYNC_Playing.notify();
+        }
+        mPlayerStatus = PLAYER_STATUS.PLAYER_IDLE;
+        mUIHandler.removeMessages(UI_EVENT_UPDATE_CURRPOSITION);
+        return true;
+    }
+
+    /**
+     * 播放完成
+     */
+    @Override
+    public void onCompletion() {
+        // TODO Auto-generated method stub
+        Log.v(TAG, "onCompletion");
+        synchronized (SYNC_Playing) {
+            SYNC_Playing.notify();
+        }
+        mPlayerStatus = PLAYER_STATUS.PLAYER_IDLE;
+        mUIHandler.removeMessages(UI_EVENT_UPDATE_CURRPOSITION);
+    }
+
+    /**
+     * 准备播放就绪
+     */
+    @Override
+    public void onPrepared() {
+        // TODO Auto-generated method stub
+        Log.v(TAG, "onPrepared");
+        mPlayerStatus = PLAYER_STATUS.PLAYER_PREPARED;
+        mUIHandler.sendEmptyMessage(UI_EVENT_UPDATE_CURRPOSITION);
+    }
+}
